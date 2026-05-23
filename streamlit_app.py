19c840f985e15779782abee9d8891bf2fde58cd4
dtadzdzaky2006 <daffadzky2006@gmail.com>
2026-05-23 09:41:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,6 @@
-import streamlit as st
+# app.py — baris 13
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
 
-st.title("🎈 Chemistry Calculation")
-
-#Input angka
-num1 = st.number_input("Masukkan angka pertama", value=0.0)
-num2 = st.number_input("Masukkan angka kedua", value=0.0)
-
-operasi = st.selectbox("Pilih operasi", ["Tambah", "Kurang", "Kali", "Bagi"])
-    
-
-if st.button("Hitung"):
-    if operasi == "Tambah":
-        hasil = num1 + num2
-    elif operasi == "Kurang":
-         hasil = num1 - num2
-    elif operasi == "Kali":
-         hasil = num1 * num2
-    elif operasi == "Bagi":
-        hasil = num1 / num2 if num2 != 0 else "Tidak bisa dibagi nol"
-
- st.success(f"Hasil: {hasil}")
